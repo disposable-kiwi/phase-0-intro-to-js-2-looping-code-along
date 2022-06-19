69994c2f003c0b291e7b1ccfe1cd3c158cb607ee
Min <biffboffkev@gmail.com>
2022-06-19 00:02:05 -0400

    Finished tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, string){
+    let thankArray = [];
+    array.forEach(element =>{
+        const card = `Thank you, ` + element +`, for the wonderful ${string} gift!`;
+        thankArray.push(card);
+    });
+    return thankArray;
+}
+
+function countDown(num){
+    let count = num;
+    while(count>=0){
+        console.log(count);
+        count--
+    }
+}
